dbd79b38d6c6621173d7aa20d8ba2c7c49006bf9
DjPaulP <Paul@paulprenderville.ie>
2021-05-01 19:25:12 +0000

    added google map api to the code and displayed it on website

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,4 +1,8 @@
 function initMap () {
-    var location = {lat: 28.2916, lng: 16.6291};
-    var map = new google.maps.Map(document.getElementById("map"), {zoom: 4, center: location});
+    var location = {lat: 28.291565, lng: -16.629129};
+    var map = new google.maps.Map(document.getElementById("map"), {zoom: 10, center: location});
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
 }
