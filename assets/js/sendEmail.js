1705a84b1fbe0cce350b01adfd3f15406185b260
DjPaulP <Paul@paulprenderville.ie>
2021-05-11 15:47:06 +0000

    added emailjs to the modal for newsletter. It now sends a serparate email for newletter sign up

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -13,3 +13,18 @@ function sendMail(contactForm){
         });
         return false;
 }
+
+function sendNews(newsForm){
+    emailjs.send("service_92vmsff","newsletter", {
+        "from_name": newsForm.name.value,
+        "from_email": newsForm.emailaddress.value,
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
+        return false;
+}
