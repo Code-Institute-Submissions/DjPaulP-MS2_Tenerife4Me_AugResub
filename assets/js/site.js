c7b00929aed94f9a441254593d6b3f6040dc145d
DjPaulP <Paul@paulprenderville.ie>
2021-08-19 13:34:21 +0000

    Add validation for name and email address on forms

--- assets/js/site.js
+++ assets/js/site.js
@@ -35,4 +35,5 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}        
+}
+
